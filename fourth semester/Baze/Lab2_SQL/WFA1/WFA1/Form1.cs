cd666f4c12cd50b1fd08624fd2f53e9db56b8f3d
Badea Vlad-Stefan <115445900+BadeaVladStefan@users.noreply.github.com>
2024-03-28 18:23:11 +0200

    w5v3

---
+++ fourth semester/Baze/Lab2_SQL/WFA1/WFA1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WFA1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlDataAdapter dataAdapterManufacturers;
+        SqlDataAdapter dataAdapterTeams;
+        DataSet dataSet;
+        SqlCommandBuilder commandBuilder;
+
+        string parentQuery;
+        string childQuery;
+        string parentTableName;
+        string childTableName;
+        string parentPrimaryKey;
+        string childForeignKey;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadSettings();
+            FillData();
+        }
+
+        void LoadSettings()
+        {
+            parentQuery = ConfigurationManager.AppSettings["ParentQuery"];
+            childQuery = ConfigurationManager.AppSettings["ChildQuery"];
+            parentTableName = ConfigurationManager.AppSettings["ParentTableName"];
+            childTableName = ConfigurationManager.AppSettings["ChildTableName"];
+            parentPrimaryKey = ConfigurationManager.AppSettings["ParentPrimaryKey"];
+            childForeignKey = ConfigurationManager.AppSettings["ChildForeignKey"];
+        }
+
+        void FillData()
+        {
+            try
+            {
+                connection = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]);
+
+                dataAdapterManufacturers = new SqlDataAdapter(parentQuery, connection);
+                dataAdapterTeams = new SqlDataAdapter(childQuery, connection);
+
+                dataSet = new DataSet();
+                dataAdapterManufacturers.Fill(dataSet, parentTableName);
+                dataAdapterTeams.Fill(dataSet, childTableName);
+
+                commandBuilder = new SqlCommandBuilder(dataAdapterTeams);
+
+                dataSet.Relations.Add(
+                    new DataRelation(
+                        "ParentChildRelation",
+                        dataSet.Tables[parentTableName].Columns[parentPrimaryKey],
+                        dataSet.Tables[childTableName].Columns[childForeignKey]
+                    )
+                );
+
+                dataGridViewManufacturers.DataSource = dataSet.Tables[parentTableName];
+                dataGridViewTeams.DataSource = dataSet.Tables[parentTableName];
+                dataGridViewTeams.DataMember = "ParentChildRelation";
+
+                commandBuilder.GetUpdateCommand();
+                commandBuilder.GetDeleteCommand();
+                commandBuilder.GetInsertCommand();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataAdapterTeams.Update(dataSet, childTableName);
+                MessageBox.Show("Changes were saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+    }
+}
