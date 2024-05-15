029bb84dd2d8a1362459754fdfb4050fe8f473fe
Badea Vlad-Stefan <115445900+BadeaVladStefan@users.noreply.github.com>
2024-05-15 09:15:16 +0300

    a

--- fourth semester/MPP/A4_MPP/A1_MPP/src/App.tsx
+++ fourth semester/MPP/A4_MPP/A1_MPP/src/App.tsx
@@ -1,18 +1,24 @@
+// App.tsx
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 import MainApp from './MainApp';
+import Navigation from './Navigation';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/app" element={<MainApp />} />
-        <Route path="/" element={<LoginForm />} /> // Default route
-      </Routes>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/app" element={<MainApp />} />
+          <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect to /login */}
+        </Routes>
+      </div>
     </Router>
   );
 };
